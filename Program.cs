82d98cccfc1c4e195d4b50778110e4beb5854da6
Kvetuse "Pein" Husakova <42646031+Yushikuni@users.noreply.github.com>
2021-02-27 10:38:22 +0100

    pridani programu
    jen pridani programu

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlepKlepKlepPenny
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ťuk");
+            Console.WriteLine("ťuk");
+            Console.WriteLine("ťuk");
+            Console.WriteLine("Penny!");
+            Console.ReadLine();
+        }
+    }
+}
